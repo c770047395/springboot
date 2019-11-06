efa6384ead34afab2b892e99de2db6674446149d
chenpeng <770047395@qq.com>
2019-11-06 14:59:12 +0800

    第一个springboot的helloworld

---
+++ springboot_demo01/src/main/java/com/cp/springboot_demo01/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cp.springboot_demo01.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello,springboot";
+    }
+
+}
---
+++ springboot_demo01/src/test/java/com/cp/springboot_demo01/SpringbootDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cp.springboot_demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
