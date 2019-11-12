60a52c958918825c419891be1a620e51f464fc0b
chenpeng <770047395@qq.com>
2019-11-12 16:24:46 +0800

    Swagger的接口/开关配置

--- swagger_demo/README.md
+++ swagger_demo/README.md
@@ -26,9 +26,12 @@ public class SwaggerConfig {
 3. 访问localhost:8080/swagger-ui.html即可进入swagger页面
 
 ## 2.配置Swagger
-配置Swagger需要注册一个Docket的bean到配置中,通过阅读源码发现需要添加一个DocumentationType类，这里使用自带的
 
-然后通过Docket.apiInfo()方法可以配置页面的一些静态信息，需要传入一个ApiInfo参数。
+配置Swagger需要注册一个Docket的bean到配置中,通过阅读源码发现需要添加一个DocumentationType类，这里使用自带的SWAGGER_2
+
+### 2.1配置基础信息
+
+通过Docket.apiInfo()方法可以配置页面的一些静态信息，需要传入一个ApiInfo参数。
 ```java
 @Configuration
 @EnableSwagger2 //开启Swagger2
@@ -56,4 +59,37 @@ public class SwaggerConfig {
     }
 }
 
+```
+
+### 2.2配置扫描接口以及开关
+这些配置也同样在Docket里配置
+
+.select()与.build()是一对方法对，中间只能加apis或者paths方法，apis是指定要扫描的包等等，paths是指定只扫描哪些
+
+enable(Boolean)方法是Swagger2的开关，这里我们可以使用Environment 获取外部配置文件
+，在不同的环境选择开/关Swagger2
+```java
+//配置了Swagger的Docket的bean实例
+@Bean
+public Docket docket(Environment environment){
+    //设置显示的swagger环境
+    Profiles profiles = Profiles.of("test","dev");
+    //通过environment.acceptsProfiles判断是否处在自己设定的环境中
+    boolean flag = environment.acceptsProfiles(profiles);
+    return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            //enable是否启用Swagger，如果为false，则不能在浏览器中访问
+            .enable(flag)
+            .select()
+            //RequestHandlerSelectors,配置扫描接口的方式
+            //basePackage()指定要扫描的包
+            //any()扫描全部
+            //none()都不扫描
+            //withClassAnnotation()扫描类上的注解
+            //withMethodAnnotation()扫描方法上的注解
+            .apis(RequestHandlerSelectors.basePackage("com.cp.swagger_demo.controller"))
+            //paths 过滤，只扫描
+            .paths(PathSelectors.ant("/**"))
+            .build();
+}
 ```
--- swagger_demo/src/main/java/com/cp/swagger_demo/config/SwaggerConfig.java
+++ swagger_demo/src/main/java/com/cp/swagger_demo/config/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.cp.swagger_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -17,9 +21,26 @@ public class SwaggerConfig {
 
     //配置了Swagger的Docket的bean实例
     @Bean
-    public Docket docket(){
+    public Docket docket(Environment environment){
+        //设置显示的swagger环境
+        Profiles profiles = Profiles.of("test","dev");
+        //通过environment.acceptsProfiles判断是否处在自己设定的环境中
+        boolean flag = environment.acceptsProfiles(profiles);
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                //enable是否启用Swagger，如果为false，则不能在浏览器中访问
+                .enable(flag)
+                .select()
+                //RequestHandlerSelectors,配置扫描接口的方式
+                //basePackage()指定要扫描的包
+                //any()扫描全部
+                //none()都不扫描
+                //withClassAnnotation()扫描类上的注解
+                //withMethodAnnotation()扫描方法上的注解
+                .apis(RequestHandlerSelectors.basePackage("com.cp.swagger_demo.controller"))
+                //paths 过滤，只扫描
+                .paths(PathSelectors.ant("/**"))
+                .build();
     }
 
     //配置Swagger信息=apiInfo
