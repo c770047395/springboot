96a393565303b2cf1b2379e91f9c064deb28b776
chenpeng <770047395@qq.com>
2019-11-12 15:49:35 +0800

    配置Swagger

--- swagger_demo/README.md
+++ swagger_demo/README.md
@@ -1,6 +1,6 @@
 # Swagger2
 
-## 1.配置Swagger2
+## 1.集成Swagger2
 
 1. 引入maven依赖
 
@@ -24,3 +24,36 @@ public class SwaggerConfig {
 }
 ```
 3. 访问localhost:8080/swagger-ui.html即可进入swagger页面
+
+## 2.配置Swagger
+配置Swagger需要注册一个Docket的bean到配置中,通过阅读源码发现需要添加一个DocumentationType类，这里使用自带的
+
+然后通过Docket.apiInfo()方法可以配置页面的一些静态信息，需要传入一个ApiInfo参数。
+```java
+@Configuration
+@EnableSwagger2 //开启Swagger2
+public class SwaggerConfig {
+
+    //配置了Swagger的Docket的bean实例
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    //配置Swagger信息=apiInfo
+    private ApiInfo apiInfo(){
+        return new ApiInfo("CP的SwaggerAPI文档",
+                "嘻嘻嘻",
+                "1.0",
+                "47.96.128.98",
+                //作者信息
+                new Contact("cp","47.96.128.98","770047395@qq.com"),
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+
+    }
+}
+
+```
--- swagger_demo/src/main/java/com/cp/swagger_demo/config/SwaggerConfig.java
+++ swagger_demo/src/main/java/com/cp/swagger_demo/config/SwaggerConfig.java
@@ -1,11 +1,38 @@
 package com.cp.swagger_demo.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2 //开启Swagger2
 public class SwaggerConfig {
 
+    //配置了Swagger的Docket的bean实例
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo());
+    }
+
+    //配置Swagger信息=apiInfo
+    private ApiInfo apiInfo(){
+        return new ApiInfo("CP的SwaggerAPI文档",
+                "嘻嘻嘻",
+                "1.0",
+                "47.96.128.98",
+                //作者信息
+                new Contact("cp","47.96.128.98","770047395@qq.com"),
+                "Apache 2.0",
+                "http://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList());
+
+    }
 }
